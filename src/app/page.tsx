7f7a12c931c072a01fdc506d0a07813fbe1f4d2f
yousef yahia <yoousefyahia@gmail.com>
2026-07-31 02:28:41 +0300

    clean main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import { AuthCard, AuthHeader, HiringPanel } from "@/components/auth/AuthShell";
-import { SignupProgress } from "@/components/auth/SignupProgress";
-import { SignupAccountForm } from "@/components/forms/SignupAccountForm";
-import styles from "@/components/auth/auth.module.css";
-import Navbar from "@/components/shared/Navbar";
-import Footer from "@/components/shared/Footer";
-export default function SignupPage() {
+// import { AuthCard, AuthHeader, HiringPanel } from "@/components/auth/AuthShell";
+// import { SignupProgress } from "@/components/auth/SignupProgress";
+// import { SignupAccountForm } from "@/components/forms/SignupAccountForm";
+// import styles from "@/components/auth/auth.module.css";
+// import Navbar from "@/components/shared/Navbar";
+// import Footer from "@/components/shared/Footer";
+export default function Home() {
   return (
-    <main  className= "flex flex-col min-h-screen  " >
-      <Navbar />
-      <Footer />
+    <main  className= "flex flex-col min-h-screen " >
+    <h1 className="text-3xl font-bold text-center mt-10">test</h1>
+   {/* <Navbar />
+      <Footer /> */}
       {/* <div className={styles.shell}>
         <AuthHeader title="Create Your Account" signup />
         <SignupProgress step={1} label="Company details" />
@@ -18,5 +19,4 @@ export default function SignupPage() {
         </AuthCard>
       </div> */}
     </main>
-  );
-}
+  );}
